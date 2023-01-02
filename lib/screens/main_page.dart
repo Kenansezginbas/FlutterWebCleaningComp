f35d993b328dd8835c5a018747263a36f9422433
kenansezginbas <kenansezginbas@hotmail.com>
2023-01-02 23:57:00 +0300

    ...

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -4,6 +4,7 @@ import 'package:cleaning_comp/screens/contact_page.dart';
 import 'package:cleaning_comp/screens/home_page.dart';
 import 'package:cleaning_comp/screens/info_page.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -14,7 +15,7 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> with NavBarList {
   String title = "Canpak Group";
-  int tabLength = 3;
+  int tabLength = 5;
 
   @override
   Widget build(BuildContext context) {
@@ -28,29 +29,27 @@ class _MainPageState extends State<MainPage> with NavBarList {
     );
   }
 
-  PreferredSize customNavBar(BuildContext context) {
+  AppBar customNavBar(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return PreferredSize(
-      preferredSize: const Size.fromHeight(70),
-      child: Container(
-        padding: const EdgeInsets.only(left: 10, right: 10),
-        color: ColorManager.red,
-        child: Row(
-          children: [
-            Text(
-              title,
-              style: TextStyle(color: ColorManager.white),
-            ),
-            const Spacer(),
-            SizedBox(
-              width: size.width * .50,
+    return AppBar(
+      backgroundColor: ColorManager.red,
+      title: const Text('Canpak Group'),
+      centerTitle: false,
+      actions: const [
+        FaIcon(FontAwesomeIcons.instagram),
+        FaIcon(FontAwesomeIcons.facebook),
+      ],
+      bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(70),
+          child: Container(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: SizedBox(
+              width: size.width * .80,
               child: TabBar(
                   indicatorColor: Colors.transparent,
                   tabs: navBarList.map((e) => Tab(text: e.title)).toList()),
-            )
-          ],
-        ),
-      ),
+            ),
+          )),
     );
   }
 
